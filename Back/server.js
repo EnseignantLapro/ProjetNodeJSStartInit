c3597f7ef1a6827e8eef38737993806bc4e44f90
Rapidecho <jlanglace@la-providence.net>
2025-12-02 15:24:37 +0100

    first

---
+++ Back/server.js
@@ -0,0 +1,20 @@
+// Chargement du module Express qui permet de créer un serveur web
+//et de gérer les requêtes HTTP
+const express = require('express');
+const path = require("path");
+
+// Création d'une instance de l'application Express
+const app =  express();
+
+app.use(express.static(path.join(__dirname, "..", "Front")));
+
+// Définition d'une route GET pour la racine ('/')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "Front", "index.html"));
+});
+
+//listen attends 2 paramètres : le port et une fonction anonyme callback
+app.listen(3000,   () => {
+  console.log('Serveur lancé sur http://localhost:3000');
+});
+
